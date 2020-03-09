9b66fa57aaedcfcd1c677fde2f17988031483988
Coding Duck <57272996+CodingDuckmx@users.noreply.github.com>
2020-03-09 18:21:39 -0400

    Demostrate the need to remove code from global scope we want to be able to import

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -7,3 +7,11 @@ print('Hello World')
 
 df = pandas.DataFrame({"state": ["CT", "CO", "CA", "TX"]})
 print(df.head())
+
+
+print("-----------------")
+
+
+x = 5
+print("NUMBER", x)
+print("ENLARGED NUMBER", enlarge(x)) # invoking our function!!
